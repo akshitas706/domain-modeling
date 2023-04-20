a21361aaa0bcf43419ef6c545108f4ad114b81f7
Akshita Singh <akshita.singh1@kellogg.northwestern.edu>
2023-04-20 23:26:40 +0000

    created one to many tables

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,10 +3,11 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 DROP TABLE IF EXISTS contacts;
-DROP TABLE IF EXISTS conmpanies;
+DROP TABLE IF EXISTS companies;
 DROP TABLE IF EXISTS salespeople;
 DROP TABLE IF EXISTS activities;
 DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industry_memberships;
 
 
 
@@ -16,16 +17,19 @@ CREATE TABLE contacts (
     first_name TEXT,
     last_name TEXT,
     email TEXT,
-    phone_number INTEGER
+    phone_number INTEGER,
+    company_id  INTEGER
 );
 
 CREATE TABLE companies (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
+    name TEXT,
+    company_id INTEGER
 );
 
 CREATE TABLE salespeople (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    salesperson_id INTEGER,
     first_name TEXT,
     last_name TEXT,
     email TEXT
@@ -34,10 +38,24 @@ CREATE TABLE salespeople (
 CREATE TABLE activities (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     notes TEXT,
-    created_at TEXT
+    created_at TEXT,
+    salesperson_id INTEGER,
+    contact_id INTEGER
 );
 
 CREATE TABLE industries (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
+    name TEXT,
+    industry_id INTEGER
+);
+
+-- industry and companies have many to many relationships
+-- an industry will have multiple companies
+-- a company can belong to multiple industries
+-- this new table will create a many to one relationship that we need in sql
+
+CREATE TABLE industry_memberships (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_id INTEGER,
+    industry_id INTEGER 
 );
