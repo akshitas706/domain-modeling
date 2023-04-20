3d179f7a653d6efde18e80454437aa6571ae78b7
Akshita Singh <akshita.singh1@kellogg.northwestern.edu>
2023-04-20 23:36:42 +0000

    started 2nd exercise

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,19 @@
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
 
+
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_name INTEGER,
+    real_name TEXT,
+    location TEXT
+
+);
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image_url TEXT,
+    posted_at TEXT,
+    user_id INTEGER
+);
